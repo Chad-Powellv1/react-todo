6f8264788a695c1ef99b276735b91a6f2c3789c3
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-22 23:43:40 +0000

    update Form.js file with state, handleSubmit, and handleChange

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Form } from './components/Form';
+import React from 'react';
 import './App.css';
 
 export const App = () => {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Form />
     </div>
   );
 }
