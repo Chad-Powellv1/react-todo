0499701992587b64847f6d60600fec2f32d2e1ee
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-22 16:38:26 +0000

    updated file structure

---
+++ src/components/Active.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Active = () => {
+    return (
+        <h1>Active</h1>
+    )
+}
---
+++ src/components/All.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const All = () => {
+    return (
+        <h1>All</h1>
+    )
+}
