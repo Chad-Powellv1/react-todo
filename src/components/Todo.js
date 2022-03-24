86c6d5f995520f8e7780e3c333e5075872d34e63
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-24 20:31:07 +0000

    add buttons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,11 @@ import { BiCheck, BiTrash } from 'react-icons/bi';
 import React from "react";
 import './todo.css';
 
-export const Todo = ( {text} ) => {
+export const Todo = ( {text, handleComplete} ) => {
 
-
-    return (
+   return (
         <div className="todo">
-            <li className="clicked" style={{display: 'flex', justifyContent:'space-between'}} >{<BiCheck/>}{text}{<BiTrash />}</li> 
+            <li style={{display: 'flex', justifyContent:'space-between'}} >{<BiCheck onClick={handleComplete}/>}{text}{<BiTrash />}</li> 
         </div>
     )
 };
